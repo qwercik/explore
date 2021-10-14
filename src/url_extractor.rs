cdd36706b581e06c22cdfe454fd699369d8f1fdb
qwercik <erykandrzejewski@gmail.com>
2021-10-14 22:17:44 +0200

    Validate URLs in url_extractor

--- src/url_extractor.rs
+++ src/url_extractor.rs
@@ -1,12 +1,24 @@
 use soup::prelude::*;
+use url::Url;
+use regex::Regex;
 
-pub fn extract_from_html(html_file_content: &str) -> Vec<String> {
+fn valid_scheme(url: &Url) -> bool {
+    Regex::new(r"https?")
+        .unwrap()
+        .is_match(url.scheme())
+}
+
+pub fn extract_from_html(html_file_content: &str) -> Vec<Url> {
     let soup = Soup::new(html_file_content);
     
     soup.tag("a")
         .find_all()
-        .map(|t| t.get("href"))
+        .map(|tag| tag.get("href"))
         .filter(Option::is_some)
         .map(Option::unwrap)
+        .map(|s| Url::parse(&s))
+        .filter(Result::is_ok)
+        .map(Result::unwrap)
+        .filter(valid_scheme)
         .collect()
 }
