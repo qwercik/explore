593abdc4bb003388e42cecaa66c187930f228ecc
qwercik <erykandrzejewski@gmail.com>
2021-10-14 21:04:16 +0200

    Create a CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name="explore",
+    about="Explore the WWW and find the shortest path between two HTML documents",
+    author="Eryk Andrzejewski"
+)]
+struct Options {
+    #[structopt(help="URL from which you would like to start exploring")]
+    start_url: String,
+
+    #[structopt(help="URL to which you would like to find a path, beginning from the start URL")]
+    final_url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::from_args();
+    println!("{:#?}", options);
 }
