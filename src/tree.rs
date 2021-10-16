66bbc1738a152771086a9e3ff14f2d2dc188e018
qwercik <erykandrzejewski@gmail.com>
2021-10-16 15:14:17 +0200

    Simplify tree module

--- src/tree.rs
+++ src/tree.rs
@@ -1,45 +1,23 @@
-pub type TreeResult<T> = Result<T, TreeError>;
-
 #[derive(Debug)]
-pub enum TreeError {
-    NodeIsEmpty,
-}
-
-#[derive(Debug)]
-pub enum Tree<T> {
-    Empty,
-    NonEmpty {
-        value: T,
-        children: Vec<Box<Tree<T>>>,
-    },
+pub struct Tree<T> {
+    value: T,
+    children: Vec<Box<Tree<T>>>
 }
 
 impl<T> Tree<T> {
-    pub fn empty() -> Self {
-        Self::Empty
-    }
-
-    pub fn new(value: T) -> Self {
-        Self::NonEmpty {
+    fn new(value: T) -> Self {
+        Self {
             value,
-            children: vec![],
+            children: vec![]
         }
     }
 
-    pub fn is_empty(&self) -> bool {
-        matches!(self, Self::Empty)
+    fn append_child_tree(&mut self, child: Self) -> &mut Tree<T> {
+        self.children.push(Box::new(child));
+        &mut **self.children.last_mut().unwrap()
     }
 
-    pub fn append_child_tree(&mut self, child: Self) -> TreeResult<&mut Tree<T>> {
-        if let Tree::NonEmpty { children, .. } = self {
-            children.push(Box::new(child));
-            Ok(&mut **children.last_mut().unwrap())
-        } else {
-            Err(TreeError::NodeIsEmpty)
-        }
-    }
-
-    pub fn append_child(&mut self, value: T) -> TreeResult<&mut Tree<T>> {
+    fn append_child(&mut self, value: T) -> &mut Tree<T> {
         self.append_child_tree(Tree::new(value))
     }
 }
@@ -48,42 +26,26 @@ impl<T> Tree<T> {
 mod test {
     use super::*;
 
-    #[test]
-    fn test_creating_empty_tree() {
-        let tree: Tree<u32> = Tree::empty();
-        assert!(tree.is_empty());
-    }
 
     #[test]
-    fn test_creating_tree_with_root() {
+    fn test_creating_tree() {
         let tree = Tree::new(997);
-        assert!(!tree.is_empty());
-
-        match tree {
-            Tree::NonEmpty { value, children } => {
-                assert_eq!(value, 997);
-                assert_eq!(children.len(), 0);
-            }
-            _ => panic!("Expected NonEmpty, got empty"),
-        }
+        assert_eq!(tree.value, 997);
+        assert_eq!(tree.children.len(), 0);
     }
 
     #[test]
     fn test_appending_values() {
-        fn func() -> TreeResult<Tree<u32>> {
-            let mut tree = Tree::new(997);
-            tree.append_child(112)?;
-            let node = tree.append_child(1410).unwrap();
-            let _ = node.append_child(777)?;
-            let _ = node.append_child(778)?;
+        let mut tree = Tree::new(997);
+        let left = tree.append_child(112);
+        left.append_child(123);
+        left.append_child(444);
+        left.append_child(23);
 
-            Ok(tree)
-        }
+        let right = tree.append_child(1410);
+        right.append_child(777);
+        right.append_child(778);
 
-        let result = func();
-        assert!(result.is_ok());
-
-        let tree = result.unwrap();
         // Use cargo test -- --show-output to see it,
         println!("{:#?}", tree);
     }
